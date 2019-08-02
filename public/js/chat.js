dd03152b1a84fd869ea4f66fa34e191537de94a7
snpr <vaugk13@gmail.com>
2019-08-02 18:17:05 +0500

    Disable send location button while location sending

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,6 +4,7 @@ const socket = io();
 const $messageForm = document.querySelector("#message-form");
 const $messageFormInput = $messageForm.querySelector("input");
 const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
 
 socket.on("message", message => {
   console.log(message);
@@ -29,11 +30,13 @@ $messageForm.addEventListener("submit", evt => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", evt => {
+$sendLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser ");
   }
 
+  $sendLocationButton.setAttribute("disabled", "disabled");
+  
   navigator.geolocation.getCurrentPosition(position => {
     socket.emit(
       "sendLocation",
@@ -42,6 +45,7 @@ document.querySelector("#send-location").addEventListener("click", evt => {
         longitude: position.coords.longitude
       },
       () => {
+        $sendLocationButton.removeAttribute("disabled");
         console.log("Location shared!");
       }
     );
