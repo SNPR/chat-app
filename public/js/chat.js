81b80a2b08dd480b29cbda9ce6dbf3882f949b8e
snpr <vaugk13@gmail.com>
2019-08-07 16:08:24 +0500

    Add autoscroll while user send new message

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,6 +19,29 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
 });
 
+const autoscroll = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild;
+
+  // Height of the new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  // Visible heigth
+  const visibleHeight = $messages.offsetHeight;
+
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have I scrolled?
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
+
 socket.on("message", message => {
   console.log(message.text);
 
@@ -28,6 +51,7 @@ socket.on("message", message => {
     createdAt: moment(message.createdAt).format("HH:mm")
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
 });
 
 socket.on("locationMessage", message => {
@@ -39,6 +63,7 @@ socket.on("locationMessage", message => {
     createdAt: moment(message.createdAt).format("HH:mm")
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
 });
 
 socket.on("roomData", ({ room, users }) => {
